5fcf185f11f68b60352ff9aa439f65c1c6c11745
Sonu <sonu108rp@gmail.com>
2024-08-27 14:46:07 +0530

    basic file created

---
+++ Training/convert.py
@@ -0,0 +1,8 @@
+#WAP to convert decimal to binary and hexadecimal
+n=14
+print(bin(n))
+print(hex(n))
+print(oct(n))
+strs='a'
+n=int(strs,16)
+print(n)
---
+++ Training/texCalculate.py
@@ -0,0 +1,5 @@
+income =1000
+print(income)
+texRate=0.1
+tex=income*texRate
+print(tex)
