f8322fb50c132b098084d93667156dbb302ec119
Sonu <sonu108rp@gmail.com>
2024-08-27 12:13:17 +0530

    training start

---
+++ Training/first.py
@@ -0,0 +1,7 @@
+# print("hello")
+a=5
+b=a
+a=7
+# print(b)
+a=(1,2)
+print(type(a))
--- loop.py
+++ loop.py
@@ -1,12 +1,12 @@
-# for i in [0,1,2,3,4,5]:
-#     print(i)
+for i in [0,1,2,3,4,5]:
+    print(i)
 # -------------
-# for i in range(11):
-#     print(i)
+for i in range(11):
+    print(i)
 # -------------
-# names=["sonu","wintr","hello"]
-# for name in names:
-#     print(name)
+names=["sonu","wintr","hello"]
+for name in names:
+    print(name)
 # -------------
 name="harry"
 for char in name:
